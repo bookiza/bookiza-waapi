1da2fa9cac41f2f13c5a6d872d3e826746cda1d1
marvindanig <marvin@marvindanig.com>
2018-07-25 21:16:24 -0400

    A not so cool solution to revrsing the animationState of a page

--- scripts/invoker.js
+++ scripts/invoker.js
@@ -1,4 +1,4 @@
-const options = { duration: 1200, peel: true, zoom: true, startPage: 6 }
+const options = { duration: 300, peel: true, zoom: true, startPage: 6 }
 
 const superbook = Bookiza.init({ options })
 
